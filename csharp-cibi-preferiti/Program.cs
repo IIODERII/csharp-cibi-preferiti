4999f1ba6f71dbc5478a06c81e9b24e571f64dfa
IIODERII <oderrisi@gmail.com>
2024-04-16 15:20:26 +0200

    Aggiungere i file di progetto.

---
+++ csharp-cibi-preferiti/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace csharp_cibi_preferiti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] favouriteFoods = { "Pizza", "Pasta", "Risotto", "Fiorentina", "Hamburger", "Sushi"};
+            Console.WriteLine($"La mia lista contiene {favouriteFoods.Length} cibi. \n");
+
+            Console.WriteLine("Ecco la mia lista:");
+
+            for(int i = 0; i< favouriteFoods.Length; i++)
+            { 
+                Console.WriteLine($"{i+1}. {favouriteFoods[i]}"); 
+            }
+
+            Console.WriteLine($"\nIl mio cibo preferito è: {favouriteFoods[0]}. \nMentre il mio cibo preferito ma non troppo è: {favouriteFoods[favouriteFoods.Length-1]}.");
+       
+            //bonus
+
+            if(favouriteFoods.Length % 2 == 0)
+            {
+                Console.WriteLine($"\nI cibi che stanno in mezzo alla mia classifica sono: {favouriteFoods[(favouriteFoods.Length / 2) - 1]} e {favouriteFoods[favouriteFoods.Length / 2]}");
+            }
+            else
+            {
+                Console.WriteLine($"\nIl cibo che sta a metà della mia classifica è {favouriteFoods[((favouriteFoods.Length + 1) / 2)-1]}");
+            }
+        }
+    }
+}
